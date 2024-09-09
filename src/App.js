3d5b298d7a7ff01d92ee9ad6771012d3488137dd
unknown <nqnhu6124@gmail.com>
2024-09-09 14:45:01 +0700

    fix conflict sidebar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/Customer/Homepage";
 import "./App.css";
 import { Box } from "@mui/material";
-import Sidebar from "./pages/Admin/Sidebar";
+import Sidebar from "./components/Sidebar";
 import { NotificationPage } from "./pages/Customer/NotificationPage";
 import { News } from "./pages/Customer/News";
 
