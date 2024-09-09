19cce79f151330b24f8a19719aca631a07ff6619
unknown <nqnhu6124@gmail.com>
2024-09-09 20:46:57 +0700

    Customer + Employee table

---
+++ src/components/EmployeeTable.js
@@ -0,0 +1,112 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Switch } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { useState } from 'react';
+
+const IOSSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme, checked }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: checked ? 'red' : '#65C466',
+        opacity: 1,
+        border: 0,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: '#33cf4d',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color: theme.palette.grey[100],
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: 0.7,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 22,
+    height: 22,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: '#E9E9EA',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
+
+function createData(stt, tennhanvien) {
+  return { stt, tennhanvien};
+}
+
+const rows = [
+  createData(1, 'EMPLOYEE01', false),
+  createData(2, 'EMPLOYEE02', false),
+  createData(3, 'EMPLOYEE03', false),
+  createData(4, 'EMPLOYEE04', false),
+  createData(5, 'EMPLOYEE05', false),
+];
+
+export default function BasicTable() {
+  const [blockStatus, setBlockStatus] = useState(
+    rows.map(row => row.chan)
+  );
+
+  const handleSwitchChange = (index) => {
+    const updatedStatus = [...blockStatus];
+    updatedStatus[index] = !updatedStatus[index];
+    setBlockStatus(updatedStatus);
+  };
+
+  return (
+    <TableContainer component={Paper} sx={{ width: '1080px' }}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left" sx={{ fontWeight: 'bold', width: '100px' }}>
+              STT
+            </TableCell>
+            <TableCell align="left" sx={{ fontWeight: 'bold', width: '600px' }}>
+              Tên Nhân viên
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, index) => (
+            <TableRow key={row.tennhanvien} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell align="left" scope="row">
+                {row.stt}
+              </TableCell>
+              <TableCell align="left">{row.tennhanvien}</TableCell>
+              
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/pages/Admin/CustomerManagement.js
+++ src/pages/Admin/CustomerManagement.js
@@ -1,10 +1,7 @@
 import React from "react";
 import {
   Box,
-  AppBar,
-  Toolbar,
   IconButton,
-  Typography,
   Button,
   TextField,
   InputAdornment,
@@ -12,11 +9,120 @@ import {
 import SearchIcon from "@mui/icons-material/Search";
 import FilterListIcon from '@mui/icons-material/FilterList';
 import styles from "./Service.module.css";
+import CustomerTable from "../../components/CustomerTable";
 
 function CustomerManagement() {
+
   return (
-    <></>
-  )
+    <>
+      <Box
+        sx={{
+          marginTop: "65px",
+          backgroundColor: "white",
+          maxWidth: "100%",
+          display: "flex",
+        }}
+      >
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            <a href="#home" className={styles.a}>
+              Tất cả khách hàng
+            </a>
+          </li>
+          <li className={styles.li}>
+            <a href="#news" className={styles.a}>
+              Nhiều đơn
+            </a>
+          </li>
+          <li className={styles.li}>
+            <a href="#contact" className={styles.a}>
+              Ít đơn
+            </a>
+          </li>
+          <li className={styles.li}>
+            <a href="#about" className={styles.a}>
+              Chưa có đơn
+            </a>
+          </li>
+          <li className={styles.li}>
+            <a href="#about" className={styles.a}>
+              Bị chặn
+            </a>
+          </li>
+        </ul>
+      </Box>
+
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, marginTop: "3px", backgroundColor: "white" }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            maxWidth: "100%",
+            marginLeft: "50px",
+          }}
+        >
+          <TextField
+            variant="outlined"
+            placeholder="Tìm kiếm khách hàng"
+            size="small"
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: "20px",
+                width: "500px",
+                margin: "0 20px 0 -0",
+              },
+            }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <Button
+            variant="outlined"
+            size="small"
+            sx={{
+              borderRadius: "20px",
+              padding: "6px 16px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              minWidth: "200px",
+              borderColor: "rgba(0, 0, 0, 0.23)", 
+              color: "rgba(0, 0, 0, 0.87)", 
+              textTransform: "none",
+              "&:hover": {
+                borderColor: "rgba(0, 0, 0, 0.87)",
+                backgroundColor: "rgba(0, 0, 0, 0.04)", 
+              },
+            }}
+            startIcon={
+              <FilterListIcon />
+            }
+          >
+            Thêm điều kiện lọc
+          </Button>
+
+        </Box>
+        <Box
+          sx={{
+            maxWidth: "100%",
+            marginLeft: "50px",
+            marginTop: "50px",
+          }}
+        >
+          <CustomerTable />
+        </Box>
+      </Box>
+    </>
+  );
 }
 
 export default CustomerManagement;
